c9c35d4b8e0336585ed55a350c9e5be70455a7df
Jhonatan Santos <jhonatansantos085@gmail.com>
2024-08-07 22:48:43 -0300

    commit inicial

---
+++ src/main/java/exercicio/sistemaCRUD/SistemaCrudApplication.java
@@ -0,0 +1,13 @@
+package exercicio.sistemaCRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/exercicio/sistemaCRUD/SistemaCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package exercicio.sistemaCRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
